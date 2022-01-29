6e3146f438478b879e84c4ab6998373289555335
Anderson Fernandes <mayhara.26@gmail.com>
2022-01-29 15:49:33 -0300

    aula2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
 import appConfig from './config.json';
+import {useRouter} from 'next/router';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,11 +35,15 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'mayhara-fernandes';
+   //const username = 'pears';
+   const [username, setUsername] = React.useState();
+   const roteamento = useRouter();
+  
+
 
   return (
     <>
-      <GlobalStyle />
+      
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,17 +70,28 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit = {function(event){
+              event.preventDefault()
+              roteamento.push('/chat');
+            }}
+
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Seja Bem-vindo !</Titulo>
+            <Titulo tag="h2"> Seja Bem-vindo !</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[999] }}>
               {appConfig.name}
             </Text>
-
-            <TextField
+        
+           
+           <TextField
+            value = {username}
+            onChange={function handler(event){
+              const valor = event.target.value;
+              setUsername(valor);
+            }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -125,7 +115,7 @@ export default function PaginaInicial() {
             />
           </Box>
           {/* Formulário */}
-
+        
 
           {/* Photo Area */}
           <Box
